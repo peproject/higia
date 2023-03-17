115a45dd2adcd005e306fb87172bff7dbe14a30f
Gabriel Mariz <gabriellucena997@outlook.com>
2023-03-16 21:01:55 -0300

    :sparkles: first commit

---
+++ src/test/java/io/github/higia/HigiaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.higia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HigiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
